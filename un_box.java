77a99cb2a3bad4f65bb81123890b30cfc6970598
payalGund2610 <150929149+payalGund2610@users.noreply.github.com>
2024-05-13 12:31:42 +0530

    Add files via upload

---
+++ un_box.java
@@ -0,0 +1,14 @@
+package Unboxing;
+
+public class un_box 
+{
+
+	public static void main(String[] args) 
+	{
+		Integer num1 = new Integer(89);
+		int num2=num1;
+		System.out.println("hii"+num2);
+
+	}
+
+}
